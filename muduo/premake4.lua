7d6a11a6fbafd9dcd86e347fdc17242d16c4188b
Ke Peng <kepeng@gmail.com>
2012-11-14 23:45:18 +0800

    premake4 add with-muduo-test option

--- muduo/premake4.lua
+++ muduo/premake4.lua
@@ -1,1 +1,31 @@
-project "muduo"    kind "StaticLib"    files {        "**.h",        "**.cc",    }    protobufs "**/*.proto"    excludes {        "**/tests/*",    }    configuration "macosx"        excludes { "**/EPollPoller.cc" }local unittests = os.matchfiles("**/tests/*.cc")for _,unittest in ipairs(unittests) do    project(path.getbasename(unittest))        kind "ConsoleApp"        files(unittest)        links {            "muduo",            "boost_unit_test_framework-mt",        }end
+newoption {
+   trigger     = "with-muduo-test",
+   description = "build with muduo tests."
+}
+
+project "muduo"
+    kind "StaticLib"
+    files {
+        "**.h",
+        "**.cc",
+    }
+    protobufs "**/*.proto"
+    excludes {
+        "**/tests/*",
+    }
+    configuration "macosx"
+        excludes { "**/EPollPoller.cc" }
+
+if _OPTIONS["with-muduo-test"] then
+    local unittests = os.matchfiles("**/tests/*.cc")
+    for _,unittest in ipairs(unittests) do
+        project(path.getbasename(unittest))
+            kind "ConsoleApp"
+            files(unittest)
+            links {
+                "muduo",
+                "boost_unit_test_framework-mt",
+            }
+    end
+end
+
