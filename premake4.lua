b9179bf545513ee708b6127b51ffdec56e77b329
Ke Peng <kepeng@gmail.com>
2012-11-24 21:47:02 +0800

    mac2unix for premake4.lua

--- premake4.lua
+++ premake4.lua
@@ -1,1 +1,48 @@
-for _,script in ipairs(os.matchfiles("premake4/*.lua")) do    dofile(script);enddefaultaction "gmake"solution "muduo"    location "build"    language "C++"    configurations { "Debug", "Release" }    targetdir "build/$(config)"    objdir "build/obj"    includedirs {        "."    }    libdirs {        "$(TARGETDIR)"    }    flags {        "Symbols",        "ExtraWarnings",        --"FatalWarnings",    }    buildoptions {        "-Wconversion",        "-Wno-unused-parameter",        "-Wold-style-cast",        "-Woverloaded-virtual",        "-Wpointer-arith",        "-Wshadow",        "-Wwrite-strings",    }    configuration "Debug"        defines { "_DEBUG" }    configuration "Release"        defines { "NDEBUG" }        flags { "Optimize" }        buildoptions {            "-finline-limit=1000",        }    configuration "macosx"        libdirs {            "/opt/local/lib",        }     include "muduo"
+for _,script in ipairs(os.matchfiles("premake4/*.lua")) do
+    dofile(script);
+end
+
+defaultaction "gmake"
+
+solution "muduo"
+    location "build"
+    language "C++"
+    configurations { "Debug", "Release" }
+    targetdir "build/$(config)"
+    objdir "build/obj"
+    includedirs {
+        "."
+    }
+    libdirs {
+        "$(TARGETDIR)"
+    }
+    flags {
+        "Symbols",
+        "ExtraWarnings",
+        --"FatalWarnings",
+    }
+    buildoptions {
+        "-Wconversion",
+        "-Wno-unused-parameter",
+        "-Wold-style-cast",
+        "-Woverloaded-virtual",
+        "-Wpointer-arith",
+        "-Wshadow",
+        "-Wwrite-strings",
+    }
+    configuration "Debug"
+        defines { "_DEBUG" }
+
+    configuration "Release"
+        defines { "NDEBUG" }
+        flags { "Optimize" }
+        buildoptions {
+            "-finline-limit=1000",
+        }
+    configuration "macosx"
+        libdirs {
+            "/opt/local/lib",
+        }
+ 
+    include "muduo"
+
